4ed12948e75f161703bc14dede7a3a9eda49aa2a
JMFrmg <matthieu.colombert@gmail.com>
2022-12-01 20:57:42 +0100

    Basic flask app creation.

---
+++ API/app/main.py
@@ -0,0 +1,10 @@
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/predict_tags')
+def index():
+    return json.dumps({'name': 'alice',
+                       'email': 'alice@outlook.com'})
+app.run()

3ec7530af1fa9c8672b17fe259fada1e1be3335a
JMFrmg <matthieu.colombert@gmail.com>
2022-12-01 23:16:16 +0100

    Predict tags endpoint creation.

--- API/app/app.py
+++ API/app/app.py
@@ -3,6 +3,8 @@ import pickle
 import json
 
 from flask import Flask, request, jsonify
+import numpy as np
+import pandas as pd
 import sklearn
 
 logger = logging.getLogger(__name__)
@@ -15,12 +17,9 @@ with open("logistic_classifier.pickle", "rb") as f:
 with open("binarizer.pickle", "rb") as f:
     binarizer = pickle.load(f)
 with open("tfidf.pickle", "rb") as f:
-    tfidf = pickle.load(f)
+    vectorizer = pickle.load(f)
 with open("token2id.pickle", "rb") as f:
-    token2id = pickle.load(f)
-
-logger.info("!!!!!!!!!!!!!!!!!!!!!")
-logger.info(type(classifier))
+    tokens_set = pickle.load(f)
 
 app = Flask(__name__)
 
@@ -28,11 +27,32 @@ app = Flask(__name__)
 def home():
     return 'Flask with docker!'
 
-@app.route("/predict_tags")
+@app.route("/predict_tags", methods=['POST'])
 def predict_tags():
-    text = request.json()
-    logger.info(f"Request text : {text}")
-    return jsonify({"text": text})
+    data = request.json
+    title = data["title"] if "title" in data.keys() else ""
+    body = data["body"] if "body" in data.keys() else ""
+    text = title + " " + body
+    logger.debug(f"Request text : {text}")
+    text = text.strip().lower()
+    doc = [word for word in text.split(" ") if word.isalnum()]
+    tokens = [token for token in doc if token in tokens_set]
+    logger.debug(f"Request tokens : {tokens}")
+    logger.debug(pd.Series([" ".join(tokens)]))
+    doc_vectorized = vectorizer.transform(pd.Series([" ".join(tokens)]))
+    logger.debug(f"Doc vectorized : {doc_vectorized[0].todense()}")
+    logger.debug(type(doc_vectorized))
+    y_pred_proba = classifier.predict_proba(doc_vectorized)
+    logger.debug(y_pred_proba)
+    y_best_preds = []
+    for pred in y_pred_proba:
+        best_preds_indexes = np.argsort(pred)[-3:]
+        results = np.zeros((pred.shape[0],))
+        results[best_preds_indexes] = 1
+        y_best_preds.append(results)
+    y_best_preds = np.array(y_best_preds)
+    tags = binarizer.inverse_transform(y_best_preds)[0]
+    return jsonify({"text": text, "tags": tags})
 
 if __name__ == '__main__':
      classifier = pickle.load("logistic_classifier.pickle")
